c1b6e544f7ad598a94c21cb286a72189d95855cc
domenico123134235 <00001113550648sp@al.educacao.sp.gov.br>
2024-11-13 14:12:47 -0300

    Add files via upload
    arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'desenho',
+    'O que é rachras?',
+    'rachuras são linhas cruzadas qu podem dar movimento e volume ao desenho'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da índia?',
+    'A capital da índia é nova dheli'
+)
+
+criaCartao(
+    'culinária',
+    'O que é um mirepoix?',
+    'é uma construção de sabores feita por vegetais'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
